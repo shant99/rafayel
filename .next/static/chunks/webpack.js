4bcdad68e408202a70d9da8804704bdfee209d15
shant99 <xxxx.shant.xxxx@gmail.com>
2022-07-01 15:51:45 +0400

    final commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f0108630f4bf491c"; }
+/******/ 		__webpack_require__.h = function() { return "8e03b2fc03d2183e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.12b0ce839b7eb196.hot-update.js
+++ .next/static/webpack/webpack.12b0ce839b7eb196.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "78468073eed5c00b"; }
+/******/ 	__webpack_require__.h = function() { return "ab58849ca331f0c2"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
